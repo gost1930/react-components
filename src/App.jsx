68ee8c0298a5b91a183e29afe60cadc1b268e283
gost1930 <hadbimohamed66@gmail.com>
2024-12-23 00:38:20 +0100

    fix dropDown route

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route , BrowserRouter as Router , Routes } from 'react-router-dom';
-import {Home , Modal , SlideBar } from './pages';
+import {Home , Modal , CustomDropDown, SlideBar } from './pages';
 
 
 const App = () => {
@@ -9,6 +9,7 @@ const App = () => {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/drop-down" element={<CustomDropDown />} />
         <Route path="/modal" element={<Modal />} />
         <Route path="/slide" element={<SlideBar />} />
       </Routes>
