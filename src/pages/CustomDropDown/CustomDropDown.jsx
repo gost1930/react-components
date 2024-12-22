2f8e305b4995f20e13fb4279c53fc4420996d643
gost1930 <hadbimohamed66@gmail.com>
2024-12-22 16:05:13 +0100

    drop down done

--- src/pages/CustomDropDown/CustomDropDown.jsx
+++ src/pages/CustomDropDown/CustomDropDown.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { IoIosArrowDown } from "react-icons/io";
-import "./style.css";
+
+
 const CustomDropDown = () => {
   const btn = [
     {
@@ -21,6 +22,24 @@ const CustomDropDown = () => {
       description:
         "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus ducimus laudantium sint adipisci, commodi aut similique amet minima. Totam reprehenderit voluptate eligendi, ut, ipsum provident pariatur in porro mollitia omnis esse cum! Accusantium dolorem aperiam, quia ex sed, obcaecati incidunt ipsa molestias placeat eos adipisci quae rem nemo nam pariatur.",
     },
+    {
+      id: 4,
+      title: "drop4",
+      description:
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus ducimus laudantium sint adipisci, commodi aut similique amet minima. Totam reprehenderit voluptate eligendi, ut, ipsum provident pariatur in porro mollitia omnis esse cum! Accusantium dolorem aperiam, quia ex sed, obcaecati incidunt ipsa molestias placeat eos adipisci quae rem nemo nam pariatur.",
+    },
+    {
+      id: 5,
+      title: "drop5",
+      description:
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus ducimus laudantium sint adipisci, commodi aut similique amet minima. Totam reprehenderit voluptate eligendi, ut, ipsum provident pariatur in porro mollitia omnis esse cum! Accusantium dolorem aperiam, quia ex sed, obcaecati incidunt ipsa molestias placeat eos adipisci quae rem nemo nam pariatur.",
+    },
+    {
+      id: 6,
+      title: "drop6",
+      description:
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus ducimus laudantium sint adipisci, commodi aut similique amet minima. Totam reprehenderit voluptate eligendi, ut, ipsum provident pariatur in porro mollitia omnis esse cum! Accusantium dolorem aperiam, quia ex sed, obcaecati incidunt ipsa molestias placeat eos adipisci quae rem nemo nam pariatur.",
+    },
   ];
 
   const [isOpen, setIsOpen] = useState(null);
@@ -28,20 +47,16 @@ const CustomDropDown = () => {
   const dropDown = (id) => {
     setIsOpen((prev) => (prev === id ? null : id));
   };
-  console.log(isOpen);
+
   return (
-    <div className="flex flex-col items-center h-screen w-screen mx-auto gap-y-5 mt-10 overflow-hidden">
+    <div className="flex flex-col items-center h-screen w-full mx-auto gap-y-5 mt-10 overflow-x-hidden">
       <h1 className="text-zinc-800 text-5xl font-bold">This is a drop down</h1>
       {/* drop down list */}
       <div className="flex flex-col w-full px-40">
         {btn.map((item, index) => (
           <div
             key={index}
-            className={`flex flex-col gap-y-3 w-full px-2 border-b py-4  duration-500 ${
-              isOpen === item.id
-                ? "dropDownAnimation"
-                : "dropDownAnimationReverse"
-            }`}
+            className="flex flex-col gap-y-3 w-full px-2 border-b py-4"
           >
             <div
               onClick={() => dropDown(item.id)}
@@ -56,7 +71,18 @@ const CustomDropDown = () => {
                 }`}
               />
             </div>
-            {isOpen === item.id && <p className="text-zinc-500">{item.description}</p>}
+            <div
+              className={`overflow-hidden transition-all duration-300 ease-in-out max-h-0 ${
+                isOpen === item.id ? "max-h-40" : ""
+              }`}
+            >
+              {isOpen === item.id && (
+                <p className="text-zinc-500 transition-colors">
+                  {item.description}
+                </p>
+                // or any content here but you should add more max hight to show all your content
+              )}
+            </div>
           </div>
         ))}
       </div>
